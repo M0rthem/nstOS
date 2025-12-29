5db843d5b2dbcac6adc85589d8f250191f89e44b
Nesterov I. B. <ilianesterov95@gmail.com>
2025-12-29 21:14:06 +0700

    Hello World!

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,11 @@
 void main() {
-  *(unsigned short *)0xb8000 = 'Q' | (0x0F << 8);
+  char *hello_str = "Hello World!";
+  unsigned short *now_pos = (unsigned short *)0xb8000;
+  for (int i = 0; i != 12; i++) {
+    *now_pos = hello_str[i] | (0x0F << 8);
+    now_pos += 1;
+  }
+
   while (1) {
   }
   return;
