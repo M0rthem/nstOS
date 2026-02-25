4bf29042a9565d64616c27004c023ad7e8bd8a3f
Nesterov I. B. <ilianesterov95@gmail.com>
2026-02-25 19:20:51 +0700

    add print count ticks

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,8 @@
 #include <stdint.h>
 
+#define MAX_LEN_UINT32 10
+void uint_to_str(uint32_t number, char *string);
+
 typedef struct __attribute__((packed)) idt_entry {
   uint16_t low_offset;
   uint16_t selector;
@@ -54,26 +57,51 @@ void setupIdt() {
   __asm__ volatile("sti");
 }
 
+void print_s(unsigned short *vga_start, char *str);
 void print_hello(void) {
   char *hello_str = "Hello World!";
-  unsigned short *now_pos = (unsigned short *)0xb8000;
-  for (int i = 0; i != 12; i++) {
-    *now_pos = hello_str[i] | (0x0F << 8);
-    now_pos += 1;
-  }
+  print_s((unsigned short *)0xb8000, hello_str); 
 }
 
 uint32_t tick = 0;
 
 void print_tick(void) {
-  char *hello_str = "tick";
-  unsigned short *now_pos = (unsigned short *)0xb80A0;
-  for (int i = 0; i != 12; i++) {
-    *now_pos = hello_str[i] | (0x0F << 8);
-    now_pos += 1;
-  }
+  char tick_str[MAX_LEN_UINT32 + sizeof("tick")] = "tick";
+  tick_str[4] = ' ';
+  uint_to_str(tick, &(tick_str[5]));
+  print_s((unsigned short *)0xb8020, tick_str);
 }
 void idt_timer_hdlr(void) {
   tick++;
   print_tick();
+  __asm__ volatile("outb %%al, %%dx" : : "a"(0x20), "d"(0x20));
+}
+
+void uint_to_str(uint32_t number, char *string) {
+  if(number == 0){
+    string[0] = '0';
+    string[1] = '\n';
+    return;
+  }
+  char buffer[MAX_LEN_UINT32];
+  unsigned char count_symbols = 0;
+  while(number != 0){
+    buffer[count_symbols] = number % 10 + 48;
+    count_symbols++;
+    number /= 10;  
+  }
+  for(int i = 0; i != count_symbols; i++){
+    string[i] = buffer[count_symbols - i - 1];
+  }  
+  string[count_symbols] = '\0';
+}
+
+void print_s(unsigned short *vga_start, char *str){ 
+  unsigned short *now_pos =(unsigned short *) vga_start;
+  char *now_char = str;
+  while (*now_char != '\0') {
+    *now_pos = *now_char | (0x0F << 8);
+    now_pos += 1;
+    now_char++;
+  }
 }
